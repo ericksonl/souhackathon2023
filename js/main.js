e4d54702f91cfe3496163318e3d595bd362eb890
Liam <ericksonl@sou.edu>
2023-03-28 15:31:55 -0700

    Finished product v1

--- js/main.js
+++ js/main.js
@@ -1,24 +1,74 @@
 import { getShakesWord } from "./elizabethDict.js"
 
-document.getElementById("generateBtn").addEventListener("click", convertText)
+document.getElementById("generateBtn").addEventListener("click", translateText) //add event listener for generate button
+document.getElementById("swapBtn").addEventListener("click", swapTranslator) //add event listener for swap button
 
-function convertText() {
-    var textOut = ""
+let englishShown = false; //a boolean to easily determine which table is being shown, i.e which translation is being used
 
-    var textIn = (document.getElementById("shakespeareTxt").innerHTML).toLowerCase()
+//swap the translations from shakespeare to english, or vise versa
+function swapTranslator() {
+    var shakespeareTable = document.getElementById("sTranslateTable")
+    var englishTable = document.getElementById("eTranslateTable")
 
-    const breakpoint = /(\.|\,|\;|\:|\!|\?|\s)/
-
-    var textArr = textIn.trim().split(breakpoint);
-    console.log(textArr)
-    for (var i = 0; i < textArr.length; i++) {
-        if (getShakesWord(textArr[i])) {
-            textOut += getShakesWord(textArr[i])
-        } else {
-            textOut += textArr[i]
-        }
+    //if English table is show, hide it and show Shakespeare table
+    if (englishTable.style.display === "none") {
+        englishTable.style.display = "block"
+        shakespeareTable.style.display = "none";
+        englishShown = true;
+    } else {
+        //if Shakespeare table is show, hide it and show English table
+        shakespeareTable.style.display = "block";
+        englishTable.style.display = "none"
+        englishShown = false;
     }
-    document.getElementById("englishTxt").innerText = textOut
 }
+
+function translateText() {
+    if (englishShown === false) {
+        var textOut = ""
+
+        var textIn = (document.getElementById("inputText").innerText).toLowerCase()
+
+        const breakpoint = /(\.|\,|\;|\:|\!|\?|\s)/
+
+        var textArr = textIn.trim().split(breakpoint);
+        console.log(textArr)
+        for (var i = 0; i < textArr.length; i++) {
+            if (getShakesWord(textArr[i])) {
+                textOut += getShakesWord(textArr[i])
+            } else {
+                textOut += textArr[i]
+            }
+        }
+        document.getElementById("outputText").innerText = textOut
+    } else {
+        var textIn = (document.getElementById("inputText2").innerText).toLowerCase()
+
+        let url = "https://api.funtranslations.com/translate/shakespeare.json";
+
+        const getUrl = (text) => {
+            return url + "?text=" + text;
+        };
+
+        const showError = (error) => {
+            alert(
+                "Uh Oh! Some error occured\n\n" + error + "\n\nPlease try after sometime."
+            );
+            console.log({ error });
+        };
+
+        fetch(getUrl(textIn))
+            .then((response) => response.json())
+            .then((json) => showOutput(json))
+            .catch(showError);
+
+
+        const showOutput = (text) => {
+            console.log(text);
+            document.getElementById("outputText2").innerHTML = text.contents.translated;
+        };
+    }
+}
+
 
 
